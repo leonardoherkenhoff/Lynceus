9c0d10df88d553bad22f03c5c56d62e5ba2a1c9a
Léo <leonardoherkenhoff@gmail.com>
2026-05-17 23:28:27 -0300

    fix: eleva MTU da interface veth para 65535 para suportar pacotes massivos do pcap sem falha de injecao

--- scripts/testbed/extract_all_pcaps.sh
+++ scripts/testbed/extract_all_pcaps.sh
@@ -26,8 +26,8 @@ echo "[*] Configurando par veth (veth0 <-> veth1) para ingestão offline..."
 ip link add veth0 type veth peer name veth1 2>/dev/null || true
 ip link set veth0 up
 ip link set veth1 up
-ip link set dev veth0 mtu 9000
-ip link set dev veth1 mtu 9000
+ip link set dev veth0 mtu 65535
+ip link set dev veth1 mtu 65535
 
 FILES=$(ls "$PCAP_DIR"/*.pcap 2>/dev/null)
 
