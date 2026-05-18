86d2bef5bb26ee8a27a9ce27c63a4cbb3a86791a
Léo <leonardoherkenhoff@gmail.com>
2026-05-18 11:52:37 -0300

    fix: remove MTU configs entirely to match develop's flawless native XDP behavior

--- scripts/testbed/run_performance_test.sh
+++ scripts/testbed/run_performance_test.sh
@@ -26,9 +26,6 @@ ip link delete veth1 2>/dev/null || true
 ip link add veth0 type veth peer name veth1
 ip link set veth0 up
 ip link set veth1 up
-ip link set dev veth0 mtu 3400
-ip link set dev veth1 mtu 3400
-
 IFACE="veth1"
 TC_IFACE="veth0"
 
